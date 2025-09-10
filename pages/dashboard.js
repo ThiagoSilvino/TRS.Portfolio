6c038ed0b9a9ec47e2780f0f1ef7615cb78f9f53
Thiago <thiago.silvino@icloud.com>
2025-09-10 15:19:10 -0600

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -5,7 +5,7 @@ export default function Dashboard() {
       <iframe
         title="Speckle Viewer"
         // paste your exact Speckle EMBED url on ONE LINE below:
-        src="https://app.speckle.systems/projects/9ea7c06f1f/models/301f093a88#embed=%7B%22isEnabled%22%3Atrue%7D"
+        src="https://app.speckle.systems/projects/f124377ec2/models/451dba6e1f#embed=%7B%22isEnabled%22%3Atrue%7D"
         style={{ width: "100%", height: "100%", border: 0 }}
         allow="fullscreen; clipboard-write; xr-spatial-tracking"
       />
