fddcd9eb56ffdf4a912ab117c3c96cc0bd45ee9c
Thiago <thiago.silvino@icloud.com>
2025-09-13 14:03:16 -0600

    Update nav-bar-foundry.js

--- components/nav-bar-foundry.js
+++ components/nav-bar-foundry.js
@@ -112,7 +112,7 @@ export default function NavBar() {
               pointerEvents: showTitle ? "auto" : "none",
             }}
           >
-            FOUNDRY
+            THE FOUNDRY COLLECTIVE
           </div>
 
           {/* Right: Hamburger */}
